e1325b997635da88ff8833bfc57b504400e4b5c4
ememosconi <ememosconi@gmail.com>
2019-09-05 21:19:17 -0300

    list full

--- ProductList/index.js
+++ ProductList/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import '../style.css'
 
-const ProductList=()=>(
-  <h2>Products</h2>
-);
+const ProductList=({products})=>{
+  return (
+    <div>
+      <h2>Products</h2>
+      {products.map(product => 
+      <div>
+        <h3>
+          <Link title={`${product.name} details`}>{product.name}</Link>
+        </h3>
+        {product.description&&<p>{`Descripcion: ${product.description}`}</p>}
+        <button>
+          Share
+        </button>
+      </div>
+        
+      )}
+    </div>
+    
+)};
 
 export default ProductList;
